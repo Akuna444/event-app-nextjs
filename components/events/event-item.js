f0863fc173e0892c32e2308aef44683f6fa80b75
Akuna444 <aklilsolomon3@gmail.com>
2023-03-02 11:16:38 +0300

    add: custom button to Link component

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,7 @@
-import Link from "next/link";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+
 import classes from "./event-item.module.css";
 
 function EventItem(props) {
@@ -19,14 +22,16 @@ function EventItem(props) {
           <div className={classes.summary}>
             <h2>{title}</h2>
             <div className={classes.date}>
+              <DateIcon />
               <time>{humanReadableDate}</time>
             </div>
             <div className={classes.address}>
+              <AddressIcon />
               <address>{formattedAddress}</address>
             </div>
           </div>
           <div className={classes.actions}>
-            <Link href={exproleLink}>Explore Events</Link>
+            <Button link={exproleLink}>Explore Events</Button>
           </div>
         </div>
       </li>
---
+++ components/ui/button.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import classes from "./button.module.css";
+
+function Button(props) {
+  return (
+    <Link className={classes.btn} href={props.link}>
+      {props.children}
+    </Link>
+  );
+}
+
+export default Button;
