06ae8bb0aa9e5c531480b74b83c241d9b42468f6
Akuna444 <aklilsolomon3@gmail.com>
2023-03-10 16:52:58 +0300

    add: eventId dynamic page components

---
+++ pages/events/[eventId].js
@@ -0,0 +1,32 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
+function EventDetail() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No Events Founds</p>;
+  }
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+        date={event.date}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
+}
+
+export default EventDetail;
