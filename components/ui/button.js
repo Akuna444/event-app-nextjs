8db980478e4eaf5ea1e2f9c571b1da4216ffdcfc
Akuna444 <aklilsolomon3@gmail.com>
2023-03-10 18:11:45 +0300

    add: separate onClick button

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,10 +2,18 @@ import Link from "next/link";
 import classes from "./button.module.css";
 
 function Button(props) {
+  if (props.link) {
+    return (
+      <Link className={classes.btn} href={props.link}>
+        {props.children}
+      </Link>
+    );
+  }
+
   return (
-    <Link className={classes.btn} href={props.link}>
+    <button onClick={props.onClick} className={classes.btn}>
       {props.children}
-    </Link>
+    </button>
   );
 }
 
