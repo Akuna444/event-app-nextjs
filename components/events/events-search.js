05425d8cef64fd430c61a254064ea4bc7c036762
Akuna444 <aklilsolomon3@gmail.com>
2023-03-10 18:06:21 +0300

    add: find-all-events functionality

---
+++ components/events/events-search.js
@@ -0,0 +1,37 @@
+import Button from "../ui/button";
+
+function EventsSearch() {
+  return (
+    <form>
+      <div>
+        <div>
+          <label htmlFor="year">Year</label>
+          <select id="year">
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="month">Month</label>
+          <select id="month">
+            <option value="1">January</option>
+            <option value="2">February</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">May</option>
+            <option value="6">June</option>
+            <option value="7">July</option>
+            <option value="8">August</option>
+            <option value="9">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
+          </select>
+        </div>
+      </div>
+      <Button>Find All Events</Button>
+    </form>
+  );
+}
+
+export default EventsSearch;
