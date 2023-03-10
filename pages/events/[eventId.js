cb6faab7afa1883919abf19de28d474d3a71fe74
Akuna444 <aklilsolomon3@gmail.com>
2023-03-10 16:35:17 +0300

    add: event-detail subcomponents

--- pages/events/[eventId.js
+++ pages/events/[eventId.js
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
+import { Fragment } from "react";
 import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/";
 
 function EventDetail() {
   const router = useRouter();
@@ -11,11 +13,7 @@ function EventDetail() {
     return <p>No Events Founds</p>;
   }
 
-  return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
-  );
+  return <Fragment></Fragment>;
 }
 
 export default EventDetail;
