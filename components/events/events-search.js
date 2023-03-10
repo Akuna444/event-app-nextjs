716e015443b019a84781254cc91ef591217c30c9
Akuna444 <aklilsolomon3@gmail.com>
2023-03-10 18:19:56 +0300

    add: styling to 'events seach' component

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,17 +1,18 @@
 import Button from "../ui/button";
+import classes from "./events-search.module.css";
 
 function EventsSearch() {
   return (
-    <form>
-      <div>
-        <div>
+    <form className={classes.form}>
+      <div className={classes.controls}>
+        <div className={classes.control}>
           <label htmlFor="year">Year</label>
           <select id="year">
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
         </div>
-        <div>
+        <div className={classes.control}>
           <label htmlFor="month">Month</label>
           <select id="month">
             <option value="1">January</option>
