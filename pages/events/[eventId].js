f0a58476b244faff0b22b5abac990bfd145eca1a
Akuna444 <aklilsolomon3@gmail.com>
2023-03-11 10:58:20 +0300

    add: Error alert to EventId component

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -4,14 +4,26 @@ import { getEventById } from "../../dummy-data";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
+import Button from "../../components/ui/button";
 
 function EventDetail() {
   const router = useRouter();
 
   const eventId = router.query.eventId;
   const event = getEventById(eventId);
+
   if (!event) {
-    return <p>No Events Found</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No Events Found</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button onClick={() => router.back()}>Go Back</Button>
+        </div>
+      </Fragment>
+    );
   }
   return (
     <Fragment>
