99d61333aaadfb26a45148d0774023f912293921
Akuna444 <aklilsolomon3@gmail.com>
2023-03-11 09:37:45 +0300

    implment: onSearch functionality

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,20 +1,33 @@
+import { use, useRef } from "react";
+
 import Button from "../ui/button";
 import classes from "./events-search.module.css";
 
-function EventsSearch() {
+function EventsSearch(props) {
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  const submittedHandler = (event) => {
+    event.preventDefault();
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    props.onSearch(selectedYear, selectedMonth);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submittedHandler}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
+          <select ref={yearInputRef} id="year">
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
         </div>
         <div className={classes.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
+          <select ref={monthInputRef} id="month">
             <option value="1">January</option>
             <option value="2">February</option>
             <option value="3">March</option>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import { useRouter } from "next/router";
 
 import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/event-list";
@@ -6,9 +7,16 @@ import EventsSearch from "../components/events/events-search";
 
 function HomePage() {
   const featuredEvent = getFeaturedEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
+
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={featuredEvent} />
     </Fragment>
   );
