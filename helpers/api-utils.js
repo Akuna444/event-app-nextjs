31dc0b7a3ebbfe69ce2e9d08fb224f298b9f18ca
Akuna444 <aklilsolomon3@gmail.com>
2023-03-20 14:02:02 +0300

    add: api-util helper

---
+++ helpers/api-utils.js
@@ -0,0 +1,21 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    "https://nextjs-b149f-default-rtdb.firebaseio.com/events.json"
+  );
+  const data = await response.json();
+
+  const events = [];
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
