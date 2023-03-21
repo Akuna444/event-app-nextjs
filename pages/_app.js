3dc6f96ef0f0551e452a2ca5e6294d49cd407c9c
Akuna444 <aklilsolomon3@gmail.com>
2023-03-21 21:55:33 +0300

    add: defualt head and metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
+import Head from "next/head";
 import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>NextJs Events</title>
+        <meta name="description" content="NextJS Events" />
+        <meta name="viewport" content="intial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
