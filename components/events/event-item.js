271488fc2db0c891ea343e94aa838a9f022d6512
Akuna444 <aklilsolomon3@gmail.com>
2023-03-21 22:38:04 +0300

    optmize: event-item images

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import Button from "../ui/button";
 import DateIcon from "../icons/date-icon";
 import AddressIcon from "../icons/address-icon";
@@ -18,7 +20,7 @@ function EventItem(props) {
   return (
     <ul>
       <li className={classes.item}>
-        <img src={"/" + image} alt={title} />
+        <Image src={"/" + image} alt={title} width={250} height={160} />
         <div className={classes.content}>
           <div className={classes.summary}>
             <h2>{title}</h2>
