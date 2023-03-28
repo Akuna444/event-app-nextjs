bcf832ad3bdcb24284e0e6ac9df5cc6c08995da1
Akuna444 <aklilsolomon3@gmail.com>
2023-03-28 13:41:34 +0300

    fetch: api from newsletter.js

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,10 +1,22 @@
-import classes from './newsletter-registration.module.css';
+import { useRef } from "react";
+import classes from "./newsletter-registration.module.css";
 
 function NewsletterRegistration() {
+  const emailInputRef = useRef();
   function registrationHandler(event) {
     event.preventDefault();
+    const userEmail = emailInputRef.current.value;
 
     // fetch user input (state or refs)
+    fetch("/api/newsletter", {
+      method: "POST",
+      body: JSON.stringify({ email: userEmail }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
     // optional: validate input
     // send valid data to API
   }
@@ -15,10 +27,11 @@ function NewsletterRegistration() {
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
           <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
+            ref={emailInputRef}
+            type="email"
+            id="email"
+            placeholder="Your email"
+            aria-label="Your email"
           />
           <button>Register</button>
         </div>
