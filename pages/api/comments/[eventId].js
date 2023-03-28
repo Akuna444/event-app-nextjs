152159bbf30605589d881abbb1fa719ce0ecdf3a
Akuna444 <aklilsolomon3@gmail.com>
2023-03-28 13:40:51 +0300

    add: comment api routes

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,37 @@
+function handler(req, res) {
+  const eventId = req.query.eventId;
+
+  if (req.method === "POST") {
+    const { name, email, text } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid Input" });
+      return;
+    }
+
+    const newComments = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+
+    res.status(201).json({ message: "Succesfull!", comment: newComments });
+  }
+
+  if (req.method === "GET") {
+    const dummyComments = [
+      { id: "c1", name: "Akuna", text: "First comment" },
+      { id: "c2", name: "Akuti", text: "Second comment" },
+    ];
+
+    res.status(200).json({ message: "Successful!", comment: dummyComments });
+  }
+}
