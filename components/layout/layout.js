08a91e3dce82ac933f2970c99f3285c04ebd80cd
Akuna444 <aklilsolomon3@gmail.com>
2023-04-01 13:15:39 +0300

    render notification components

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,25 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 
+import Notification from "../ui/notification";
+import NotificationContext from "../../store/notification-context";
 import MainHeader from "./main-header";
 
 function Layout(props) {
-  return <Fragment>
-    <MainHeader/>
-    <main>{props.children}</main>;
+  const NotificationCtx = useContext(NotificationContext);
+  const activeNotification = NotificationCtx.notifiction;
+  return (
+    <Fragment>
+      <MainHeader />
+      <main>{props.children}</main>;
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </Fragment>
+  );
 }
 
 export default Layout;
